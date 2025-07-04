d608c17eb317fb354a86419bded333cb65fa301c
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2025-07-04 11:33:41 +0900

    update: applicant

---
+++ BackendServer/src/application/application.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApplyEnsemble } from 'src/application/entities/apply-ensemble.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([ApplyEnsemble])
+  ]
+})

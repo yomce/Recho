02a24986200cf1157cc80b735ccda5dca7743a9a
dltldn333 <dltldn333@naver.com>
2025-07-08 01:52:07 +0900

    feat: 바이닐 배경 검은색 적용

--- WebFrontend/src/api/index.ts
+++ WebFrontend/src/api/index.ts
@@ -1,17 +1,12 @@
-import axios from "axios";
+import axiosInstance from "../services/axiosInstance";
 import type { Video } from "../types/video";
-
-const apiClient = axios.create({
-  // Vite 프록시 설정을 사용하므로 '/api'를 기본 경로로 설정합니다.
-  baseURL: "/api",
-});
 
 // likes나 createdAt으로 정렬 할 듯
 // 무한 스크롤과 간단한 추천 시스템 추가 필요
 
 export const getVideos = async (page = 1, limit = 10): Promise<Video[]> => {
   try {
-    const response = await apiClient.get<Video[]>("/videos", {
+    const response = await axiosInstance.get<Video[]>("/videos", {
       params: {
         sortBy: "createdAt",
         page,
@@ -24,5 +19,3 @@ export const getVideos = async (page = 1, limit = 10): Promise<Video[]> => {
     return [];
   }
 };
-
-export default apiClient;
--- WebFrontend/src/routes/AppRouter.tsx
+++ WebFrontend/src/routes/AppRouter.tsx
@@ -28,7 +28,7 @@ import PracticeRoomPage from "@/pages/practiceRoom/PracticeRoomPage";
 import CreatePracticeRoom from "@/pages/practiceRoom/CreatePracticeRoomPage";
 import PracticeRoomDetailPage from "@/pages/practiceRoom/PracticeRoomDetailPage";
 import UpdatePracticeRoomPage from "@/pages/practiceRoom/UpdatePracticeRoomPage";
-import UpdateRecruitEnsemblePage from '@/pages/ensemble/UpdateRecruitEnsemblePage';
+import UpdateRecruitEnsemblePage from "@/pages/ensemble/UpdateRecruitEnsemblePage";
 import MapViewPage from "@/pages/map/MapViewPage";
 import ProtectedRoute from "./ProtectedRoute";
 
@@ -51,17 +51,44 @@ const AppRouter: React.FC = () => {
           <Route path="/main" element={<MainPage />} />
           <Route path="/vinyl" element={<VinylPage />} />
           <Route path="/used-products" element={<UsedProductPage />} />
-          <Route path="/used-products/create" element={<CreateUsedProductPage />} />
-          <Route path="/used-products/:id" element={<UsedProductDetailPage />} />
-          <Route path="/used-products/edit/:id" element={<UpdateUsedProductPage />} />
+          <Route
+            path="/used-products/create"
+            element={<CreateUsedProductPage />}
+          />
+          <Route
+            path="/used-products/:id"
+            element={<UsedProductDetailPage />}
+          />
+          <Route
+            path="/used-products/edit/:id"
+            element={<UpdateUsedProductPage />}
+          />
           <Route path="/ensembles" element={<RecruitEnsembleListPage />} />
-          <Route path="/ensembles/create" element={<CreateRecruitEnsemblePage />} />
-          <Route path="/ensembles/edit/:id" element={<UpdateRecruitEnsemblePage />} />
-          <Route path="/ensembles/:id" element={<RecruitEnsembleDetailPage />} />
+          <Route
+            path="/ensembles/create"
+            element={<CreateRecruitEnsemblePage />}
+          />
+          <Route
+            path="/ensembles/edit/:id"
+            element={<UpdateRecruitEnsemblePage />}
+          />
+          <Route
+            path="/ensembles/:id"
+            element={<RecruitEnsembleDetailPage />}
+          />
           <Route path="/practice-room" element={<PracticeRoomPage />} />
-          <Route path="/practice-room/create" element={<CreatePracticeRoom />} />
-          <Route path="/practice-room/:id" element={<PracticeRoomDetailPage />} />
-          <Route path="/practice-room/edit/:id" element={<UpdatePracticeRoomPage />} />
+          <Route
+            path="/practice-room/create"
+            element={<CreatePracticeRoom />}
+          />
+          <Route
+            path="/practice-room/:id"
+            element={<PracticeRoomDetailPage />}
+          />
+          <Route
+            path="/practice-room/edit/:id"
+            element={<UpdatePracticeRoomPage />}
+          />
           <Route path="/map-view" element={<MapViewPage />} />
           <Route path="/style-guide" element={<StyleGuideTest />} />
         </Route>

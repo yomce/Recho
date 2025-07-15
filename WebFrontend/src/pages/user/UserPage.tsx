9016cb0768ce4890902ade8f70de4c4fd19a5d73
deutan <dsb000918@gmail.com>
2025-07-16 01:26:47 +0900

    fix: userpage thumb

--- WebFrontend/src/pages/user/UserPage.tsx
+++ WebFrontend/src/pages/user/UserPage.tsx
@@ -80,6 +80,10 @@ const UserPage: React.FC = () => {
           id: `thumb-${index}`,
           thumbnailUrl: url,
         }));
+
+        console.log('API 응답 후 포맷팅된 썸네일 배열:', formattedThumbnails);
+        console.log('썸네일 배열의 길이:', formattedThumbnails.length);
+
         setThumbnails(formattedThumbnails);
         setError(null); // 다른 유저 페이지 로딩 성공 시 에러 상태 초기화
       } catch (err) {

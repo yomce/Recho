edbb03bf20160fe7aaa0980e482424fb279feae4
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2025-07-18 16:35:41 +0900

    fix: user info and dto

--- BackendServer/src/application/dto/applier-ensemble.response.dto.ts
+++ BackendServer/src/application/dto/applier-ensemble.response.dto.ts
@@ -1,21 +1,17 @@
-import { UserResponseDto } from 'src/auth/user/dto/user.response.dto';
 import { SessionEnsemble } from 'src/ensemble/session/entities/session-ensemble.entity';
 import { ApplierEnsemble } from '../entities/applier-ensemble.entity';
 
 export class ApplierEnsembleResponseDto {
   applicationId: number;
-  user: UserResponseDto;
   sessionEnsemble: SessionEnsemble;
 
   static from(
     applier: ApplierEnsemble,
-    userResponseDto: UserResponseDto,
     sessionEnsemble: SessionEnsemble,
   ): ApplierEnsembleResponseDto {
     const dto = new ApplierEnsembleResponseDto();
 
     dto.applicationId = applier.applicationId;
-    dto.user = userResponseDto;
     dto.sessionEnsemble = sessionEnsemble;
 
     return dto;

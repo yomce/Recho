799b6e6e959ddef7832688978608d7f93474d1af
jyjww <imabatmanithink@gmail.com>
2025-07-21 17:00:01 +0900

    fix: 불필요한 파일, 주석 제거

--- WebFrontend/src/hooks/getImageUrl.ts
+++
@@ -1,7 +0,0 @@
-export const getImageUrl = (keys: string[] | string | undefined): string[] => {
-  if (!keys) return [];
-  if (typeof keys === 'string') return [`https://recho-img.s3.ap-northeast-2.amazonaws.com/${keys}`];
-  return keys.map(k => `https://recho-img.s3.ap-northeast-2.amazonaws.com/${k}`);
-};
-
-export default getImageUrl;
